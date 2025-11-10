4ee257706a6fa03500075b6a675252eb09d2d23e
Arath Reyes <arath.reyes@uabc.edu.mx>
2025-11-10 13:09:43 -0800

    Comentarios

--- Decoder Xor.py
+++ Decoder Xor.py
@@ -1,30 +1,36 @@
 # ======================================================
-# Autor: Arath Reyes González
-# Universidad Autónoma de Baja California
+# Autor: Arath Reyes Gonzalez
+# Universidad Autonoma de Baja California
 # Práctica: Desencriptado XOR
 # ======================================================
 
 # Tramas en hexadecimal
+#Aqqui meter todas las tramas de paquetes que se llegan a recibir, ya se da por hecho que hasta el a0 al ser iguales es Paco
 tramas = [
     "5041434f3a012d2f2d",
     "5041434f3a0a2d2e23"
 ]
 
+
 # Llave XOR
 key = 0x42
 
-# Función para convertir hex -> bytes -> aplicar XOR
+
+# Funcion para convertir hex -> bytes -> aplicar XOR
 def xor_decode(hex_data, key):
     data = bytes.fromhex(hex_data)
     result = bytes([b ^ key for b in data])
     return result
+
 
 # Procesar cada trama
 for i, trama in enumerate(tramas, 1):
     decodificada = xor_decode(trama, key)
     print(f"Trama {i}: {trama}")
     print(" Resultado XOR:", decodificada)
+    
     # Filtrar bytes imprimibles (ASCII)
     texto_legible = ''.join(chr(b) for b in decodificada if 32 <= b <= 126)
     print(" Texto legible:", texto_legible)
     print("-" * 40)
+
